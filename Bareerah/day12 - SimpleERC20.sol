ad57bf4f2bab267a729e8af0ba7f223591e5ac1c
海椰 Bareerah <99247545+BareerahBenjamin@users.noreply.github.com>
2026-03-12 12:31:58 +0800

    feat: modify Day12

--- Bareerah/day12 - SimpleERC20.sol
+++ Bareerah/day12 - SimpleERC20.sol
@@ -55,10 +55,10 @@ contract SimpleERC20{
 }
 
 // 使用openzeppelin
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
-    }
-}
+// contract MyToken is ERC20 {
+//     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+//         _mint(msg.sender, initialSupply * 10 ** decimals());
+//     }
+// }
